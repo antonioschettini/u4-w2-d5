daf559834be2dbec175e804826674e2840abe66f
antonio.schettini <antonio.schettini93@gmail.com>
2026-06-12 10:47:52 +0200

    commit add task 4

---
+++ src/main/java/antonioschettini/entities/Collezione.java
@@ -0,0 +1,60 @@
+package antonioschettini.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collezione {
+    //Attributi
+    private List<Gioco> listaGiochi; // utilizzo la classe super Gioco per essere generico tanto estende anche video e tavola;
+
+    //COstruttori
+    public Collezione() {
+        this.listaGiochi = new ArrayList<>(); // nel costruttore appena instazio una nuova collezione mi crea una arraylist vuota
+    }
+
+    //Metodi
+    //Task1, aggiunta di un elemento e non posso inserire un elemento con lo stesso id
+    public void aggiungiGioco(Gioco nuovoGioco) {
+        boolean idGiaEsistente = listaGiochi.stream() // apro lo stream
+                .anyMatch(gioco -> gioco.getIdGioco().equals(nuovoGioco.getIdGioco()));
+        // utilizzo l'anymatch per lambda predicate per ogni gioco prendi l'id e contrtolla se è uguale al gioco creato
+        // passato come parametro all'invocazione del metodo, uso i getter per recuperare l'id.
+
+        //attivo i controlli se l'id è già esistente lancio l'errore
+        if (idGiaEsistente) {
+            throw new IllegalArgumentException("Impossibile inserire, poichè esiste già un gioco con lo stesso id " + nuovoGioco.getIdGioco());
+        }
+        // se passa il controllo
+        listaGiochi.add(nuovoGioco);
+        System.out.println("Gioco inserito avrà l'id : " + nuovoGioco.getIdGioco());
+    }
+
+    //Task2, ricerco un gioco tramite id
+    public Gioco cercaperId(Long id) {  //passo Gioco poichè il metodo mi restituisce un gioco, non posso farlo void
+        // utilizzo un ciclo for
+        for (Gioco gioco : listaGiochi) {
+            // attivo i controlli se il gioco ciclato ha l'id passato nel parametro il ciclo si ferma subito
+            if (gioco.getIdGioco().equals(id)) {
+                return gioco; //
+            }
+        }
+        throw new RuntimeException("Non ho trovato nessun gioco con ID: " + id);
+    }
+
+    //Task3 ricerca per prezzo, inferiore a quello fornito nel parametro del metodo
+    public List<Gioco> cercaPerPrezzoMenoCostosoDi(double prezzomassimo) {
+        return listaGiochi.stream()
+                .filter(gioco -> gioco.getPrezzo() < prezzomassimo).toList(); // avvio lo stream effettuo un filter per ogni gioco prendimi
+        //il prezzo e comparamelo con il prezzo inserito nell parametro del metodo e poi inseriscimelo nella lista che ritorno.
+    }
+
+    //Task4 ricerca per numero giocatori
+    public List<GiocoDaTavola> ricercaPerNumeroGiocatori(Integer numeroGiocatori) {
+        return listaGiochi.stream() //Avendo una lista generica confronto prima se è una istanza di giochi da tavolo
+                .filter(gioco -> gioco instanceof GiocoDaTavola)
+                .map(gioco -> (GiocoDaTavola) gioco) //effettuo un casting per trattarlo come gioco tavola
+                .filter(giocoDaTavola -> giocoDaTavola.getNumeroGiocatori().equals(numeroGiocatori)).toList();
+        // effettuo un fiilter per ogni giocodatavola prendine i giocatore e confronta se è uguale al numero interito
+    }
+    
+}
--- src/main/java/antonioschettini/entities/GiocoDaTavola.java
+++ src/main/java/antonioschettini/entities/GiocoDaTavola.java
@@ -3,10 +3,11 @@ package antonioschettini.entities;
 import java.time.LocalDate;
 
 public class GiocoDaTavola extends Gioco {
+    //Attrbuti
     private Integer numeroGiocatori;
     private Integer durataPartita;
 
-
+    //Costruttori
     public GiocoDaTavola(String titolo, LocalDate dataPubblicazione, Double prezzo, Integer numeroGiocatori, Integer durataPartita) {
         super(titolo, dataPubblicazione, prezzo);
         this.durataPartita = durataPartita;
@@ -14,8 +15,6 @@ public class GiocoDaTavola extends Gioco {
             throw new IllegalArgumentException("Il numero di giocatori deve essere tra 2 e 10"); // utilizzo lo stesso controllo fatto per il prezzo
         }
         this.numeroGiocatori = numeroGiocatori;
-
-
     }
 
     public Integer getNumeroGiocatori() {
